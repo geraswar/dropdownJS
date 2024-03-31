05e6c3c049a2e6e425abfdf84730fdfbc071d37f
geraswar <geraswarner@gmail.com>
2024-03-31 11:45:31 +0700

    first commit

---
+++ tyy.js
@@ -0,0 +1,15 @@
+function btnclick() {
+    document.getElementById("showdropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.btnjs')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+   }
+}
